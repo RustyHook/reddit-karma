28a2587bbb16d95b2295ae1e64c7dcb4b5a86195
RustyHook <amcornerstone@bk.ru>
2016-04-30 23:44:59 +0200

    Moved part of the code to a separate func

--- reddit_karma.py
+++ reddit_karma.py
@@ -2,65 +2,69 @@ import json
 import urllib2
 import sys
 
-
-#Loop as long as user wants to compare
-while True:
-
-	print "Welcome to reddit-karma!\nA comparison tool for users' most recent posts' score\n"
-
-	username1 = raw_input("Please enter the name of the first user for comparison: ")
-
+def getJSON(username):
+	""" str -> dictcd
+	
+	Returns JSON object of users dashboard.
+	
+	Arguments:
+		username {[str]} -- A string containing a username
+	"""
 	#Try to grab the JSON object, handles 404 and 429(I guess reddit doesn't like this)
 	while True:
 		try:
-			url1 = "https://reddit.com/u/"+username1+".json"
-			data1 = json.load(urllib2.urlopen(url1))
+			url = "https://reddit.com/u/"+username+".json"
+			data = json.load(urllib2.urlopen(url))
 			break
 		except urllib2.HTTPError, err:
 			if str(err.code) == "429":
 				continue
 			else:
 				print "Invalid username, try again!"
-				username1 = raw_input("Please enter the name of the first user for comparison: ")
+				username = raw_input("Please enter the name of the first user for comparison: ")
 				continue
 
-	username2 = raw_input("Please enter the name of the second user for comparison: ")
+	return data
 
+def run():
+
+	#Loop as long as user wants to compare
 	while True:
-		try:
-			url2 = "https://reddit.com/u/"+username2+".json"
-			data2 = json.load(urllib2.urlopen(url2))
-			break
-		except urllib2.HTTPError, err:
-			if str(err.code) == "429":
-				continue
-			else:
-				print "Invalid username, try again!"
-				username2 = raw_input("Please enter the name of the second user for comparison: ")
-				continue
 
-	#Extracts karma value of the users' most-recent posts
-	karma1 = data1["data"]["children"][0]["data"]["score"]
-	karma2 = data2["data"]["children"][0]["data"]["score"] 
+		print "Welcome to reddit-karma!\nA comparison tool for users' most recent posts' score\n"
 
-	print "\n" + username1 +" received " + str(karma1) + " karma for his most-recent post, while " + username2 + " got " + str(karma2) 
+		username1 = raw_input("Please enter the name of the first user for comparison: ")
 
-	if karma1 > karma2:
-		print "\n" + username1 + " got more karma, with difference being " + str(karma1-karma2) + " points.\n"
-	elif karma2 > karma1:
-		print "\n" + username2 + " got more karma, with difference being " + str(karma2-karma1) + " points.\n"
-	else:
-		print "\n Both users have the same amount of karma for their post. It's a tie!\n"
+		data1 = getJSON(username1)
 
-	while True:
-		query = raw_input("Do you want to compare another 2 users?[Y/N]\n").lower()
+		username2 = raw_input("Please enter the name of the second user for comparison: ")
 
-		if query == "y":
-			print "\n"
-			break
-		elif query == "n":
-			print "Have a nice day ^_^"
-			sys.exit(0)
+		data2 = getJSON(username2)
+		
+		#Extracts karma value of the users' most-recent post
+		karma1 = data1["data"]["children"][0]["data"]["score"]
+		karma2 = data2["data"]["children"][0]["data"]["score"] 
+
+		print "\n" + username1 +" received " + str(karma1) + " karma for his most-recent post, while " + username2 + " got " + str(karma2) 
+
+		if karma1 > karma2:
+			print "\n" + username1 + " got more karma, with difference being " + str(karma1-karma2) + " points.\n"
+		elif karma2 > karma1:
+			print "\n" + username2 + " got more karma, with difference being " + str(karma2-karma1) + " points.\n"
 		else:
-			print "Invalid input\n"
-			continue
+			print "\n Both users have the same amount of karma for their post. It's a tie!\n"
+
+		while True:
+			query = raw_input("Do you want to compare another 2 users?[Y/N]\n").lower()
+
+			if query == "y":
+				print "\n"
+				break
+			elif query == "n":
+				print "Have a nice day ^_^"
+				sys.exit(0)
+			else:
+				print "Invalid input\n"
+				continue
+
+run()
